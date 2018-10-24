68b87096e8e691f0ff2ae01dbb8fa2c262cfb5c2
Badhreesh <badhreesh2493@gmail.com>
2018-10-24 17:21:33 +0200

    Normalization occurs in nature_cnn() now

--- policies.py
+++ policies.py
@@ -7,7 +7,7 @@ def nature_cnn(unscaled_images, **conv_kwargs):
     """
     CNN from Nature paper.
     """
-    scaled_images = tf.cast(unscaled_images, tf.float32) # / 255. # Rmoving normalisation here bcus its already done in the doom envs in doom_env.pu
+    scaled_images = tf.cast(unscaled_images, tf.float32) / 255. 
     activ = tf.nn.relu
     h = activ(conv(scaled_images, 'c1', nf=32, rf=8, stride=4, init_scale=np.sqrt(2),
                    **conv_kwargs))
