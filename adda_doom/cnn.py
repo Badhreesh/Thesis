4acee0994bcda175c5d01300d75398b02ae7eb32
Badhreesh <badhreesh2493@gmail.com>
2019-03-19 12:48:37 +0100

    Add nature_cnn w/o fc layer

--- adda_doom/cnn.py
+++ adda_doom/cnn.py
@@ -18,3 +18,19 @@ def nature_cnn(unscaled_images, scope, **conv_kwargs):
         h3 = activ(conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), **conv_kwargs))
         h3 = conv_to_fc(h3)
         return activ(fc(h3, 'fc1', nh=512, init_scale=np.sqrt(2)))
+
+@register_model_fn('nature_cnn_no_fc')
+def nature_cnn_no_fc(unscaled_images, scope, **conv_kwargs):
+    """
+    CNN from Nature paper without FC Layer
+    """
+    #unscaled_images = tf.placeholder(tf.float32, shape=[None, 84, 84, 1], name='unscaled_images')
+    with tf.variable_scope(scope):
+        scaled_images = tf.cast(unscaled_images, tf.float32) / 255. 
+        activ = tf.nn.relu
+        # 8x8 filter size is common on the very 1st conv layer, looking at the input image
+        h = activ(conv(scaled_images, 'c1', nf=32, rf=8, stride=4, init_scale=np.sqrt(2), **conv_kwargs))
+        h2 = activ(conv(h, 'c2', nf=64, rf=4, stride=2, init_scale=np.sqrt(2), **conv_kwargs))
+        h3 = activ(conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2), **conv_kwargs))
+        h3 = conv_to_fc(h3)
+        return h3
