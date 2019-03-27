5b7136439bea6d48478d9f79c18a06857293c000
Badhreesh <badhreesh2493@gmail.com>
2019-03-27 16:57:40 +0100

    Add ADDA Functionality

--- a2c/run_doom.py
+++ a2c/run_doom.py
@@ -9,14 +9,14 @@ from baselines.ppo2.policies import CnnPolicy
 '''
 create 16 envs to train a particular doom level for set no of time steps with specified model(cnn) and learning rate schedule(linear)
 '''
-def train(doom_lvl, num_timesteps, seed, policy, lrschedule, num_env, training):
+def train(doom_lvl, num_timesteps, seed, policy, lrschedule, num_env, adda_lr, adda_batch, training, use_adda):
     print('train() called')
     if policy == 'cnn':
         policy_fn = CnnPolicy
 
     env = make_doom_env(doom_lvl, num_env, seed) # Make "num_env" environments 
 
-    learn(policy_fn, env, seed, training, total_timesteps=int(num_timesteps * 1.1), lrschedule=lrschedule)  
+    learn(policy_fn, env, seed, training, use_adda, adda_lr, adda_batch, total_timesteps=int(num_timesteps * 1.1), lrschedule=lrschedule)  
     env.close()
 
 def main():
@@ -24,13 +24,16 @@ def main():
                                 # train() )
     parser.add_argument('--policy', help='Policy architecture', default='cnn')
     parser.add_argument('--lrschedule', help='Learning rate schedule', choices=['constant', 'linear'], default='linear')
+
+    parser.add_argument('--adda_lr', help='ADDA Learning Rate', default=0.0002)
+    parser.add_argument('--adda_batch', help='ADDA Batch Size', default=100)
     
     args = parser.parse_args()
-    logger.configure('/misc/student/raob/Tests/60e6_Steps/Exp_hg_normal_many_textures')
+    logger.configure('/misc/student/raob/Tests/60e6_Steps/Exp_hg_normal_with_ADDA')
     # Train "num_env" envs, each running "doom_lvl" for "num_timesteps" timesteps with a policy architecture "policy"
     # with a Learning rate schedule "lrschedule"
     train(doom_lvl=args.doom_lvl, num_timesteps=args.num_timesteps, seed=args.seed,
-        policy=args.policy, lrschedule=args.lrschedule, num_env=1, training=False) # 1) Train 16 envs 
+        policy=args.policy, lrschedule=args.lrschedule, adda_lr=args.adda_lr, adda_batch=args.adda_batch, num_env=16, training=True, use_adda=True)
 
 if __name__ == '__main__':
     main()
